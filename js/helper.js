9953346709e4a91dd68b105238d9d969e6d5f2f2
A.Emad <a.emad02018@gmail.com>
2021-06-25 23:33:20 +0200

    Adding the Home Page

---
+++ js/helper.js
@@ -0,0 +1,59 @@
+var list_element = '<li class="%class%">%data%</li>';
+var HTMLul = '<ul></ul>'
+var a_element = '<a target="_blank" href="%href%" class="%class%">%data%</a>';
+var div_element = '<div class="%class%"></div>';
+var nav_fixed = '<nav class="navbar navbar-expand-lg %color% fixed-top "><div class="container-fluid "><ul id="navelements" class="navbar-nav d-flex justify-content-around"></ul></div></nav>';
+var HTMhx = '<h%x%>%data%</h%x%>'
+var HTMLlinkButton = ' <a target="_blank" class="btn btn-%color% p-3" href="%href%" role="button">%data%</a>'
+
+var HTMLorgInfo = '<div id="%id%" class="col-sm-3  align-items-md-baseline"></div>';
+var HTMLjobInfo = '<section id="%id%"  class="col-sm-9 fs-5" ></section>';
+var HTMLPic = '<figure><img src="%data%" alt="%alt%"></figure>';
+
+var HTMCertificateInfo = '<div id="%id%" class="col-sm-6 border border-dark"></div>';
+var HTMLCertificateDetailsButton = '<button type="button" class="btn btn-primary row" data-bs-toggle="modal" data-bs-target="%id%">%data%</button>';
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span>%data%</span><hr>';
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic" alt="personal pic">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%">';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
---
+++ js/index.js
@@ -0,0 +1,304 @@
+
+var bio = {
+    "name": "Abdelrahman Emad",
+    "role": "SW Engineer <br> AI & Deep Learning Enthusiast",
+    "contacts": {
+        "mobile": "+201100436648",
+        "email": "a.emad02018@gmail.com",
+        "github": "https://github.com/Emad2018",
+        "linkedIn": "https://www.linkedin.com/in/abdelrahman-emadeldeen/",
+        "location": "Cairo,Egypt",
+        "timezone": "GMT+2"
+    },
+    "about": "Hello my Friend and welcome in my personal Website 👋<br> <br> I'm Abdelrahman Emad, A Mechatronics Engineer with a demonstrated" +
+        " history of working in the automotive industry. <br> <br>Skilled in Python, C/C++," +
+        " Computer Vision,ML and Sensor Fusion,.<br><br>" +
+        "A Bachelor's degree holder focused in Mechatronics Engineering from Ain Shams University in Cairo.<br><br>" +
+        "when I started working on this website,It was for making a real project for my HTML,CSS and JS self-study 😂.<br>"
+        + "But now I am trying to share my knowladge and  adding as much as I can of my projects that I have done to help other learners.",
+    "skills": [],
+    "bioPic": "images/myimage.jpg"
+};
+var education = {
+    "schools": [
+        {
+            "name": "Ain Shams University",
+            "city": "Cairo",
+            "degree": "Bachelor",
+            "major": "Mecatronics",
+            "graduationDate": "2013-2018",
+            "logo": "images/ainshams.png",
+            "website": "https://eng.asu.edu.eg/",
+            "graduationProjectTitle": "Advanced Driver Assistance System",
+            "graduationProjectThesis": "files/ADAS thesis.pdf"
+        }
+    ],
+    "certificates": [
+        {
+            "title": "Machine Learning Engineer Nanodegree",
+            "school": "Udacity",
+            "dates": "April-2021",
+            "programUrl": "https://www.udacity.com/course/machine-learning-engineer-nanodegree--nd009t",
+            "certificatePic": "images/certificates/MLND.jpg",
+            "certificateId": "https://confirm.udacity.com/VCHEK59N",
+            "description": "Learn advanced machine learning techniques and algorithms and how to package and deploy your models to a production environment. Gain practical experience using Amazon SageMaker to deploy trained models to a web application and evaluate the performance of your models. A/B test models and learn how to update the models as you gather more data, an important skill in industry. "
+
+        },
+        {
+            "title": "Self-Driving Car Engineer Nanodegree",
+            "school": "Udacity",
+            "dates": "Jan-2021",
+            "programUrl": "https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013",
+            "certificatePic": "images/certificates/SDND.jpg",
+            "certificateId": "https://confirm.udacity.com/CKJJCYQR",
+            "description": "You'll first apply computer vision and deep learning to automotive problems, including detecting lane lines, predicting steering angles, and more. Next, you'll learn sensor fusion, which you'll use to filter data from an array of sensors in order to perceive the environment. Finally, you'll have an opportunity to run your code in a simulation on Udacity's self-driving car. "
+        },
+        {
+            "title": "Sensor Fusion Nanodegree",
+            "school": "Udacity",
+            "dates": "Nov-2020",
+            "programUrl": "https://www.udacity.com/course/sensor-fusion-engineer-nanodegree--nd313",
+            "certificatePic": "images/certificates/SFND.jpg",
+            "certificateId": "https://confirm.udacity.com/ECKCNMKP",
+            "description": "Learn to detect obstacles in lidar point clouds through clustering and segmentation, apply thresholds and filters to radar data in order to accurately track objects, and augment your perception by projecting camera images into three dimensions and fusing these projections with other sensor data. Combine this sensor data with Kalman filters to perceive the world around a vehicle and track objects over time."
+
+        },
+        {
+            "title": "Intro to Self-Driving Cars Nanodegree",
+            "school": "Udacity",
+            "dates": "Aug-2020",
+            "programUrl": "https://www.udacity.com/course/intro-to-self-driving-cars--nd113",
+            "certificatePic": "images/certificates/ISDND.jpg",
+            "certificateId": "https://confirm.udacity.com/AKKFHCMS",
+            "description": "In this program, you’ll sharpen your Python skills, apply C++, apply matrices and calculus in code, and touch on computer vision and machine learning. These concepts will be applied to solving self-driving car problems. At the end, you’ll be ready for our Self-Driving Car Engineer Nanodegree program!"
+
+        },
+        {
+            "title": "Computer Vision Nanodegree",
+            "school": "Udacity",
+            "dates": "Aug-2020",
+            "programUrl": "https://www.udacity.com/course/computer-vision-nanodegree--nd891",
+            "certificatePic": "images/certificates/CVND.jpg",
+            "certificateId": "https://confirm.udacity.com/GKZMGA4M",
+            "description": "Learn cutting-edge computer vision and deep learning techniques—from basic image processing, to building and customizing convolutional neural networks. Apply these concepts to vision tasks such as automatic image captioning and object tracking, and build a robust portfolio of computer vision projects."
+
+        },
+        {
+            "title": "AI Programming with Python Nanodegre",
+            "school": "Udacity",
+            "dates": "Aug-2020",
+            "programUrl": "https://www.udacity.com/course/ai-programming-python-nanodegree--nd089",
+            "certificatePic": "images/certificates/AIPYTHON.jpg",
+            "certificateId": "https://confirm.udacity.com/PWJAHH4C",
+            "description": "Learn the essential foundations of AI: the programming tools (Python, NumPy, PyTorch), the math (calculus and linear algebra), and the key techniques of neural networks (gradient descent and backpropagation)."
+
+        },
+        {
+            "title": "C++ Nanodegree",
+            "school": "Udacity",
+            "dates": "July-2020",
+            "programUrl": "https://www.udacity.com/course/c-plus-plus-nanodegree--nd213",
+            "certificatePic": "images/certificates/CPLUSPLUSND.jpg",
+            "certificateId": "https://confirm.udacity.com/VGMGPHHF",
+            "description": "Get hands-on experience by coding five real-world projects. Learn to build a route planner using OpenStreetMap data, write a process monitor for your computer, and implement your own smart pointers. Finally, showcase all your newfound skills by building a multithreaded traffic simulator and coding your own C++ application."
+
+        },
+
+        {
+            "title": "FWD Web Development Challenger Track",
+            "school": "Udacity",
+            "dates": "Oct-2020",
+            "programUrl": "https://egfwd.com/web/",
+            "certificatePic": "images/certificates/FWDWEB1.jpg",
+            "certificateId": "https://graduation.udacity.com/confirm/QXEPD2XJ",
+            "description": "the web specialization track is an opportunity to develop your programming skills gradually through a fully integrated and stackable learning journey. The program starts by introducing Javascript concepts and slowly progressing towards React to become a front-end developer."
+
+        }
+    ]
+};
+var work = {
+    "jobs":
+        [
+            {
+                "employer": "Avelabs",
+                "title": "SW Engineer II",
+                "dates": "May 2019-Present",
+                "description": "Full-time",
+                "tasks":
+                    ["Creation of NVM blocks using DaVinci Developer and Davinci Configurator.",
+                        "Implement different types of Indication flags to detect any failures in the NVM operations .",
+                        "Creation of SWCs for Application Layer and RTE Ports using Vector DaVinci Developer.",
+                        "Perform (Unit Testing), Using CANoe, and Lauterbach Trace32 tools for fixing the software bugs.",
+                        "Dealing with JIRA and Plastic tool for tracking the tickets and apply the process of branch merging."
+                    ],
+                "employerLogo": "images/Avelabs-Egypt-9253-1543344533-og.png"
+            }
+
+        ],
+    "internships":
+        [
+            {
+                "employer": "Avelabs",
+                "title": "Automotive SW Development Bootcamp",
+                "dates": "Feb 2019-May 2019",
+                "description": "internship",
+                "tasks":
+                    ["Get hands-on experience by coding real-life projects using Embedded C.",
+                        "Create drivers for HW Interfacing and Communication protocols.",
+                        "Learn data structures and algorithms by solving practice problems.",
+                        "attend sessions about Makefiles,ARM Architecture, Software Engineering & Practices",
+                    ],
+                "employerLogo": "images/Avelabs-Egypt-9253-1543344533-og.png"
+            }
+
+        ]
+}
+
+var jobIds = [];
+var certificatesIds = [];
+function display_header() {
+    $("#headerName p").append(bio.name);
+    $("#headerName h3").append(bio.role);
+
+    $("#social-logos .github-logo a").attr("href", bio.contacts.github);
+    $("#social-logos .linkedin-logo a").attr("href", bio.contacts.linkedIn);
+    $("#social-logos .gmail-logo a").attr("href", "mailto:" + bio.contacts.email);
+
+
+}
+function display_about() {
+    var replacedText = "";
+    replacedText = HTMLbioPic.replace("%data%", bio.bioPic)
+    $("#bioPic").append(replacedText);
+    $("#about section p").append(bio.about);
+
+}
+
+function display_jobs(jobtype) {
+    var replacedText = "";
+    for (var i = 0; i < work[jobtype].length; i++) {
+        var classid = jobtype + i;
+        var orginfo = classid + "-orgInfo";
+        var jobInfo = classid + "-jobInfo";
+        var currentJob = work[jobtype][i];
+
+        replacedText = HTMLorgInfo.replace("%id%", orginfo);
+        $('#' + jobtype).append(replacedText);
+
+        replacedText = HTMLPic.replace("%data%", currentJob.employerLogo);
+        $('#' + orginfo).append(replacedText);
+
+        replacedText = HTMLjobInfo.replace("%id%", jobInfo);
+        $('#' + jobtype).append(replacedText);
+
+        replacedText = HTMhx.replace("%x%", "3").replace("%data%", currentJob.title);
+        $('#' + jobInfo).append(replacedText);
+        replacedText = HTMhx.replace("%x%", "4").replace("%data%", currentJob.description + ' (' + currentJob.dates + ')');
+        $('#' + jobInfo).append(replacedText);
+        $('#' + jobInfo).css({ "margin-bottom": "100px" })
+
+        $('#' + jobInfo).append(HTMLul);
+
+        for (var j = 0; j < currentJob.tasks.length; j++) {
+            replacedText = list_element.replace('%class%', "task" + j).replace('%data%', currentJob.tasks[j]);
+            $('#' + jobInfo + " ul").append(replacedText);
+        }
+    }
+}
+function display_schools() {
+    var replacedText = "";
+    for (var i = 0; i < education.schools.length; i++) {
+        var classid = "schoole" + i;
+        var schoolLogo = classid + "-logo";
+        var schoolinfo = classid + "-info";
+        var currentschool = education.schools[i];
+
+        replacedText = HTMLorgInfo.replace("%id%", schoolLogo);
+        $('#schools').append(replacedText);
+
+        replacedText = HTMLPic.replace("%data%", currentschool.logo);
+        $('#' + schoolLogo).append(replacedText);
+
+        replacedText = HTMLjobInfo.replace("%id%", schoolinfo);
+        $('#schools').append(replacedText);
+
+
+        replacedText = HTMhx.replace("%x%", "3").replace("%data%", currentschool.degree + " of " + currentschool.major);
+        $('#' + schoolinfo).append(replacedText);
+        replacedText = a_element.replace("%href%", currentschool.website).replace('%data%', currentschool.name);
+        replacedText = HTMhx.replace("%x%", "5").replace("%data%", replacedText + ", " + currentschool.city + ' (' + currentschool.graduationDate + ')');
+        $('#' + schoolinfo).append(replacedText);
+        replacedText = HTMhx.replace("%x%", "5").replace("%data%", "Graduation Project: " + currentschool.graduationProjectTitle);
+        $('#' + schoolinfo).append(replacedText);
+        replacedText = HTMhx.replace("%x%", "5").replace("%data%", "Project Grade: Excellent");
+        $('#' + schoolinfo).append(replacedText);
+        replacedText = HTMLlinkButton.replace("%color%", "primary").replace("%href%", currentschool.graduationProjectThesis).replace('%data%', 'Download the project Thesis');
+        $('#' + schoolinfo).append(replacedText);
+
+        $('#' + schoolinfo).css({ "margin-bottom": "100px" })
+    }
+}
+function create_model_html(id, title, img, school, dates, content, phref, chref) {
+    var replacedText = "";
+    replacedText += '<div class="modal fade" id=' + id + ' tabindex="-1" aria-hidden="true">';
+    replacedText += '<div class="modal-dialog modal-dialog-scrollable modal-lg"><div class="modal-content"><div class="modal-header">';
+    replacedText += '<h5 class="modal-title">' + title;
+    replacedText += '</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
+    replacedText += '</div> <div class="modal-body">'
+    replacedText += HTMLPic.replace("%data%", img).replace("%alt%", title);
+
+    replacedText += '<P>' + school + ", " + dates + '</P>' + content + '</div><div class="modal-footer">';
+    replacedText += '<button type="button" class="btn btn-secondary p-3" data-bs-dismiss="modal">Close</button>';
+    replacedText += HTMLlinkButton.replace("%color%", "primary").replace("%href%", phref).replace('%data%', 'Program Details');
+    replacedText += HTMLlinkButton.replace("%color%", "primary").replace("%href%", chref).replace('%data%', 'Graduation Certification');
+
+    replacedText += '</div></div></div></div>';
+
+    return replacedText;
+}
+
+function display_certificates() {
+    var replacedText = "";
+    for (var i = 0; i < education.certificates.length; i++) {
+        var classid = "certificates" + i;
+        var currentCertificate = education.certificates[i];
+        replacedText = HTMCertificateInfo.replace("%id%", classid);
+        $('#certificates').append(replacedText);
+
+        replacedText = HTMLPic.replace("%data%", currentCertificate.certificatePic).replace("%alt%", currentCertificate.title);
+        $('#' + classid).append(replacedText);
+
+
+
+        replacedText = HTMLCertificateDetailsButton.replace("%id%", "#" + classid + 'button').replace("%data%", currentCertificate.title + ' Details');
+        $('#' + classid + ' figure').append(replacedText);
+
+        replacedText = create_model_html(classid + 'button', currentCertificate.title, currentCertificate.certificatePic, currentCertificate.school
+            , currentCertificate.dates, currentCertificate.description, currentCertificate.programUrl, currentCertificate.certificateId);
+        $('#models').append(replacedText);
+
+    }
+
+}
+
+
+display_header();
+display_about();
+display_jobs("jobs");
+display_jobs("internships");
+display_schools();
+display_certificates();
+
+
+$('#certificates div').on("mouseleave", function (event) {
+
+    // $(this).css({ "background-color": "rgba(255, 255, 255, 0)" });
+});
+
+$('#certificates div').on("mousemove", function (event) {
+    // $(this).css({ "background-color": "rgba(0, 0, 0, 0.5)" });
+});
+
+$('#certificates div').on("click", function (event) {
+    //$(this).css({ "background-color": "rgba(255, 0, 0, 0.5)" });
+});
